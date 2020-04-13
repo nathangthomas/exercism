8e90527d949cbfd1a6a43b089ba4b285ffac0af2
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-13 01:12:39 -0600

    Add solutions to fizz_buzz and fibanacci number

--- code_challenges.rb
+++ code_challenges.rb
@@ -21,3 +21,43 @@ def two_sum(nums, target)
 end
 
 # ------------------------------------------------------------------------------
+
+# Fizz Buzz
+
+# Write a program that outputs the string representation of numbers from 1 to n.
+#
+# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+def fizz_buzz(n)
+    numbers = [*1..n]
+    new_array = []
+    numbers.map do |num|
+        if num % 3 == 0 && num % 5 == 0
+            new_array << "FizzBuzz"
+        elsif  num % 3 == 0
+            new_array << "Fizz"
+        elsif num % 5 == 0
+            new_array << "Buzz"
+        else
+          new_array << num.to_s
+        end
+    end
+    return new_array
+end
+
+# ------------------------------------------------------------------------------
+
+# Fibanacci Number
+#
+# The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+# F(0) = 0,   F(1) = 1
+# F(N) = F(N - 1) + F(N - 2), for N > 1.
+# Given N, calculate F(N).
+
+def fib(n)
+    if n == 0 || n == 1
+        return n
+    else
+        fib(n-1) + fib(n-2)
+    end
+end
