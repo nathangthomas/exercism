05820fcae88c825e0dacdd3ae70ad9caa82a3933
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-09-18 17:31:27 -0500

    Adds Java FizzBuzz

---
+++ code_challenges.java
@@ -0,0 +1,23 @@
+// Fizz Buzz
+public class Main {
+  static void fizzBuzz() {
+    int n = 100;
+    for (int i=1; i<n; i++) {
+      if (i%15==0){
+        System.out.println("FizzBuzz"+" ");
+      } else if (i%5==0){
+        System.out.println("Buzz"+" ");
+      } else if (i%3==0){
+        System.out.println("Fizz"+" ");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    fizzBuzz();
+  }
+}
+
+// PLUNGE CUT METHOD HINGE CALCULATOR
